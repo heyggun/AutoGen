c29f3475066683796e0f4d8902ac7e76ca747214
gunny <70179998+heyggun@users.noreply.github.com>
2025-04-15 15:41:44 +0900

    Delete AutoGen directory

--- AutoGen/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import SecretStr
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class Settings(BaseSettings):
-    openai_api_key: SecretStr
-
-
-settings = Settings()
