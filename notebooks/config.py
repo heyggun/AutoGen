56ef9df45f7f43c5ef625e21ecb90291cbea3d63
gunny <70179998+heyggun@users.noreply.github.com>
2025-04-29 16:12:16 +0900

    Add files via upload

---
+++ notebooks/config.py
@@ -0,0 +1,10 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    openai_api_key: SecretStr
+    anthropic_api_key: SecretStr
+
+
+settings = Settings()
